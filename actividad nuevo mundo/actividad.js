8dde5ee60ac71835037681252aa80a73a72c6933
luzma700 <40371630+luzma700@users.noreply.github.com>
2026-04-09 20:12:59 -0400

    Add files via upload

---
+++ actividad nuevo mundo/actividad.js
@@ -0,0 +1,11 @@
+// 1. Alerta emergente
+alert("Hola Mundo");
+
+// 2. Mensaje en el documento (DOM)
+document.getElementById("mensaje-doc").textContent = "Hola Mundo desde el documento";
+
+// 3. Impresión en consola
+console.log("Hola Mundo desde la consola");
+
+// 4. Mensaje en <div>
+document.getElementById("mensaje-div").innerHTML = "Hola Mundo dentro del div";
